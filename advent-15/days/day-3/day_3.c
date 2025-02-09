81c45f2119938e6cec31f20253230190d26c5d9a
Karidus-423 <kapuerto23@gmail.com>
2025-02-09 15:57:26 +0000

    2015 day 3 part1 done

--- advent-15/days/day-3/day_3.c
+++ advent-15/days/day-3/day_3.c
@@ -1,40 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-void _checkCardinal(char dir, int *n, int *s, int *w, int *e) {
 
-    switch (dir) {
-    case '^':
-        *n += 1;
-        break;
-    case 'v':
-        *s += 1;
-        break;
-    case '<':
-        *w += 1;
-        break;
-    case '>':
-        *e += 1;
-        break;
+typedef struct {
+    int x;
+    int y;
+} Santa;
+
+typedef struct {
+    int x;
+    int y;
+} Robo_Santa;
+
+typedef enum {
+    LEFT,
+    UP,
+    DOWN,
+    RIGHT,
+} Direction;
+
+int Travel(int maze[1000][1000], Direction *list, int list_size) {
+    Santa santa = {500, 500};
+    int newly_visited = 0;
+
+    for (int i = 0; i < list_size; i++) {
+        printf("%d", list[i]);
     }
+
+    for (int n = 0; n < list_size; n++) {
+        if (maze[santa.y][santa.x] == 0) {
+            newly_visited++;
+            maze[santa.y][santa.x] += 1;
+        }
+        switch (list[n]) {
+        case LEFT:
+            santa.x -= 1;
+            break;
+        case UP:
+            santa.y += 1;
+            break;
+        case DOWN:
+            santa.y -= 1;
+            break;
+        case RIGHT:
+            santa.x += 1;
+            break;
+        }
+    }
+
+    return newly_visited;
+};
+
+// Read from input.txt and return an array of directions.
+Direction *GetDirections(FILE *stream, int size) {
+
+    Direction *list = malloc(size * sizeof(Direction));
+
+    char ch;
+
+    int i = 0;
+    while ((ch = fgetc(stream)) != EOF) {
+        switch (ch) {
+        case '>':
+            list[i] = RIGHT;
+            break;
+        case '^':
+            list[i] = UP;
+            break;
+        case '<':
+            list[i] = LEFT;
+            break;
+        case 'v':
+            list[i] = DOWN;
+            break;
+        }
+        i++;
+    }
+
+    return list;
 }
 
 int main() {
-    FILE *file_ptr = fopen("./input.txt", "r");
-    if (file_ptr == NULL) {
+    FILE *stream = fopen("./input.txt", "r");
+    if (stream == NULL) {
         printf("Unable to find file.");
-        exit(0);
     }
-    char c;
-    int north = 0, south = 0, east = 0, west = 0;
-    int house_once = 1;
-    int house_many = 1;
+    fseek(stream, 0, SEEK_END);
+    int size = ftell(stream);
+    rewind(stream);
 
-    while ((c = fgetc(file_ptr)) != EOF) {
-        _checkCardinal(c, &north, &south, &east, &west);
-    }
+    Direction *directions = GetDirections(stream, size);
 
-    printf("Went North: %d, South: %d, East: %d, West: %d\n", north, south,
-           east, west);
+    fclose(stream);
 
-    fclose(file_ptr);
+    int maze[1000][1000] = {0};
+
+    int total = Travel(maze, directions, size);
+
+    free(directions);
+    printf("Total Houses:%d", total);
 }
